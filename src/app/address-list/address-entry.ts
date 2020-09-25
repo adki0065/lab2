9842a8d57084c81904b866caec5be990be92e99c
adki0065 <55300025+adki0065@users.noreply.github.com>
2020-09-24 22:26:28 -0400

    added 4 components

--- src/app/address-list/address-entry.ts
+++ src/app/address-list/address-entry.ts
@@ -4,13 +4,22 @@ export class AddressEntry {
   public phone?: string;
   public email?: string;
   public notes?: string;
+  public street?: string;
+  public city?: string;
+  public country?: string;
+  public postal?: string;
 
 
-  constructor(firstName?: string, lastName?: string, phone?: string, email?: string, notes?: string) {
+  constructor(firstName?: string, lastName?: string, phone?: string, email?: string, notes?: string,
+              street?: string, city?: string, country?: string, postal?: string) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.phone = phone;
     this.email = email;
     this.notes = notes;
+    this.street = street;
+    this.city = city;
+    this.country = country;
+    this.postal = postal;
   }
 }
